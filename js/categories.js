3597ace2f07ace147f79a10be434c885bcb8843c
Abdulaziz <abizlancer@gmail.com>
2024-01-29 10:27:01 +0500

    feat: icons font and generating categories from js

--- js/categories.js
+++ js/categories.js
@@ -1,128 +1,196 @@
-export const categories = [
+
+const categoriesContent = document.querySelector('.categories__content')
+const categories = [
   {
-    imgUrl: 'assets/images/categories1.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category1.svg',
     title: 'Для женщин',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
+    dropdown: [
+      {
+        title: 'Смартфоны',
+        linkUrl: '#!',
+        inner: {}
+      },
+      {
+        title: 'Фитнес браслеты',
+        linkUrl: '#!',
+        inner: {}
+      },
+      {
+        title: 'Аксессуары для смартфонов и телефонов',
+        linkUrl: '#!',
+        inner: {}
+      },
+      {
+        title: 'Аксессуары для смартфонов и телефонов',
+        linkUrl: '#!',
+        inner: {}
+      },
+      {
+        title: 'Ремешки для часов и смарт-часов',
+        linkUrl: '#!',
+        inner: {}
+      },
+      {
+        title: 'Смарт-часы',
+        linkUrl: '#!',
+        inner: {}
+      },
+      {
+        title: 'Запчасти для смартфонов',
+        linkUrl: '#!',
+        inner: {}
+      },
+    ]
   },
   {
-    imgUrl: 'assets/images/categories2.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category2.svg',
     title: 'Красота',
-    text: '574 объявлений'
+    text: '574 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories3.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category3.svg',
     title: 'Бытовая техника',
-    text: '547 объявлений'
+    text: '547 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories4.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category4.svg',
     title: 'Для мужчин',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories5.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category5.svg',
     title: 'Здоровье',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories6.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category6.svg',
     title: 'Электроника',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories7.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category7.svg',
     title: 'Детские товары',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories8.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category8.svg',
     title: 'Украшения и бижутерия',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories9.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category9.svg',
     title: 'Домашняя утварь',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories10.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category10.svg',
     title: 'Обувь',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories11.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category11.svg',
     title: 'Канцелярия',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories12.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category12.svg',
     title: 'Спорт и отдых',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories13.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category13.svg',
     title: 'Автотовары',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories14.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category14.svg',
     title: 'Новые легковые автомобили',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories15.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category15.svg',
     title: 'Мототранспорт',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories16.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category16.svg',
     title: 'Дача, сады и огороды',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories17.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category17.svg',
     title: 'Личная гигиена',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories18.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category18.svg',
     title: 'Аксессуарлар',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories19.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category19.svg',
     title: 'Бытовая химия и личная гигиена',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories20.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category20.svg',
     title: 'Строительство и ремонт',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
   {
-    imgUrl: 'assets/images/categories21.svg',
-    linkUrl: '#!',
+    imgUrl: 'assets/images/categories/category21.svg',
     title: 'Сумки и чемоданы',
-    text: '4 147 объявлений'
+    text: '4 147 объявлений',
   },
 ]
+
+// generate categories items
+categories.forEach((category, index) => {
+  const categoryItem = `
+  <!-- categories-item -->
+  <article class="categories__item relative item-border rounded-xl pr-3 pl-11 py-[22px] flex items-center justify-end gap-6 shadow-md w-[336px] cursor-pointer">
+                
+    <div class="categories__item--img absolute top-[50%] -left-9 -translate-y-[50%] w-16 h-16 item-border rounded-xl flex items-center justify-center shadow-md transition duration-300">
+      <img src="${category.imgUrl}" alt="category">
+    </div>
+
+    <div class="categories__item--info flex flex-col flex-grow gap-1">
+      <h4 class="text-base/[119%] font-semibold">${category.title}</h4>
+      <p class="text-sm text-gray-300">${category.text}</p>
+    </div>
+
+    <button class="icon-chevron-right transition duration-300 before:text-slate-300"></button>
+
+  </article>
+  `
+  categoriesContent.insertAdjacentHTML('beforeend', categoryItem)
+})
+
+const dropdown = `
+<div class="dropdown col-span-3 w-full grid grid-cols-3 gap-x-3 gap-y-6 p-5 bg-white border border-slate-200 rounded-2xl">
+  <a href="#!" class="dropdown__item icon-chevron-right p-3 flex flex-row-reverse items-center gap-[10px]">
+    <h5 class="flex-grow text-sm text-black font-medium">Смартфоны</h5>
+  </a>
+</div>
+`
+
+
+const categoriesItems = document.querySelectorAll('.categories__item')
+
+categoriesItems.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    // check if item contains active
+    if(item.classList.contains('active')) {
+      item.classList.remove('active')
+    } else {
+      // remove active from all categories items then add to clicked item
+      categoriesItems.forEach((item) => {
+        item.classList.remove('active')
+      })
+      
+      item.classList.toggle('active')
+    }
+  })
+})
+
---
+++ js/components.js
@@ -0,0 +1,106 @@
+const body = document.querySelector('body')
+
+
+// components
+const header = `
+<!-- header -->
+<header class=" py-3 md:py-5 sticky top-0 z-40 bg-white/[0.92] group before:absolute before:top-0 before:z-[-1] before:w-full before:h-full before:backdrop-blur-[6px]">
+  <div class="container">
+
+    <!-- header content -->
+    <div class="flex justify-between items-center">
+
+      <!-- header contacts -->
+      <div class="flex items-center gap-6">
+
+        <div class="cursor-pointer icon-chevron-down before:text-black before:w-6 before:h-6 text-sm/[130%] text-bold text-black flex flex-row-reverse items-center gap-1">
+        <span class="text-sm font-medium hidden md:block">Русский</span>
+          <img src="assets/images/ru-lang.png" alt="lang">
+          <span class="text-sm font-medium uppercase md:hidden">Ру</span>
+        </div>
+
+        <a href="tel:+998885005000" class="hidden icon-phone before:text-slate-400 md:flex text-sm/[130%] text-bold text-black items-center gap-2">
+          +998 88 500 50 00
+        </a>
+
+      </div>
+
+      <!-- header logo -->
+      <a href="#!" class="absolute left-[50%] -translate-x-[50%] translate-y-4 md:translate-y-3 px-3.5 p-3 md:py-4 bg-white border border-gray-200 rounded-[20px] md:rounded-b-[20px] md:rounded-t-none group-hover:shadow-lg transition duration-300 ">
+        <img src="assets/images/logo.png" class="w-24 md:w-full" alt="logo">
+      </a>
+
+      <!-- header links -->
+      <div class="flex items-center gap-3 md:gap-4">
+        <a href="#!" class="btn btn-secondary icon-heart before:text-blue-400 md:bg-transparent md:rounded-none md:p-0 hover:bg-transparent">
+          <span class="hidden md:block">Избранные</span>
+        </a>
+
+        <span class="hidden md:block w-[1px] h-[32px] bg-gray-100 hidden md:block"></span>
+
+        <a href="#!" class="btn btn-secondary icon-login before:text-black flex-row-reverse">
+          <span class="hidden md:block">Войти</span>
+        </a>
+      </div>
+    </div>
+    <!-- header content -->
+  </div>
+</header>
+<!-- header end -->
+`
+const footer = `
+<!-- footer -->
+<footer class="w-full relative md:h-64 bg-white">
+  <!-- footer background image -->
+  <img src="assets/images/footer-bg.png" class="rounded-[10px] absolute top-0 left-0 w-full h-full object-cover" alt="footer-bg">
+
+  <!-- footer content -->
+  <div class="container relative flex flex-col h-full pt-14 pb-6 md:p-0 items-center justify-center text-center">
+
+    <img src="assets/images/logo.png" class="w-[122px] absolute top-0 left-[47.5%] -translate-y-[50%] -translate-x-7 shadow-blue" alt="logo">
+
+    <p class="text-sm md:text-base text-black max-w-[662px] w-full">77.uz marketplace - это виртуальная платформа в Узбекистане, которая облегчает покупки и продажи, связывая продавцов с потенциальными клиентами.</p>
+
+    <!-- footer-list -->
+    <nav class="mt-6">
+      <ul class="flex flex-wrap items-center justify-center md:justify-center gap-4 md:gap-[50px]">
+
+        <li class="flex items-center gap-1">
+          <img src="assets/images/footer-symbol.svg" alt="symbol">
+          <a href="#!" class="text-xl text-black font-semibold ">Доска объявлений</a>
+        </li>
+
+        <li class="flex items-center gap-1">
+          <img src="assets/images/footer-symbol.svg" alt="symbol">
+          <a href="#!" class="text-xl text-black font-semibold ">Условия пользования</a>
+        </li>
+
+        <li class="flex items-center gap-1">
+          <img src="assets/images/footer-symbol.svg" alt="symbol">
+          <a href="tel:+998885005000" class="text-xl text-black font-semibold ">+998 88 500 50 00</a>
+        </li>
+
+      </ul>
+    </nav>
+    <!-- footer-list end -->
+  </div>
+  <!-- footer content end -->
+
+  <!-- footer-bottom -->
+  <div class="relative border-t-[1px] py-[14px] border-gray-200 bg-white">
+    <div class="container flex items-center justify-between">
+      <p class="text-sm/[114%] text-black">© 77.uz 2023. Все права защищены</p>
+
+      <a href="https://uic.group/" class="flex items-center gap-2 group">
+        <img src="assets/images/uic-logo.svg" alt="uic-logo">
+        <img src="assets/images/uic-logo-text.svg" class="hidden md:flex opacity-0 translate-x-3 group-hover:opacity-100 group-hover:translate-x-0 transition duration-300" alt="uic-logo">
+      </a>
+    </div>
+  </div>
+  <!-- footer-bottom end -->
+</footer>
+<!-- footer end -->
+`
+
+body.insertAdjacentHTML('beforebegin', header)
+body.insertAdjacentHTML('beforeend', footer)
