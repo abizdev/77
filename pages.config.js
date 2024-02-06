618fa241c3d9aa27cc31aee7dffc07c13594cc64
Abdulaziz <abizlancer@gmail.com>
2024-02-06 14:00:42 +0500

    feat: pages config file

---
+++ pages.config.js
@@ -0,0 +1,8 @@
+import { resolve } from 'path'
+
+const pages = [
+    {name: 'categories', path: resolve(__dirname, '/src/pages/categories.html')},
+    {name: 'product', path: resolve(__dirname, '/src/pages/product.html')},
+]
+
+export default pages
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
+import pages from "./pages.config";
+import {rollupVersion} from "vite";
 
+// const pagesInput = {}
+// pages.forEach((page) => {
+//   pagesInput[page.name] = page.path
+// })
 export default {
   plugins: [
     ViteWebfontDownload([
       "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap",
     ]),
   ],
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       ...pagesInput
+  //     }
+  //   }
+  // }
 };
